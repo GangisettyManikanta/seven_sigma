53ba785dbf0495a21e21c752a38317943a213beb
sai <saimamidala333@gmail.com>
2024-03-22 18:23:38 +0530

    i have modified login and signup codes

---
+++ server.py
@@ -0,0 +1,13 @@
+import anvil.server
+
+@anvil.server.callable
+def share_email(email):
+    # Save the email directly as a global variable
+    globals()['email_user'] = email
+    return email
+
+@anvil.server.callable
+def another_method():
+    # Retrieve the email directly from the global variable
+    email_user = globals().get('email_user', None)
+    return email_user
